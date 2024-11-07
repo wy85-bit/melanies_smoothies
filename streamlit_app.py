6c78199dce5d315135059d39f41229e9a572c1b2
wy85-bit <149866791+wy85-bit@users.noreply.github.com>
2024-11-07 16:28:04 +0100

    added request package and code

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+ name_on_order +"!", icon="✅")
 
 
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True) 
 # st.write(my_insert_stmt)
